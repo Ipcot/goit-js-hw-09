d88004907fb23c82c465c491b4528dc876aa2a25
Yuriy Pokhyl <woully@gmail.com>
2022-05-07 11:40:20 +0300

    task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,33 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
-console.log("r");
+let timerId = null;
+
+refs.btnStart.addEventListener('click', toStartSwitchColor);
+refs.btnStop.addEventListener('click', toStopSwitchColor);
+
+refs.btnStop.disabled = true;
+
+function toStartSwitchColor() {
+  toColorBody();
+  timerId = setInterval(toColorBody, 1000);
+}
+
+function toStopSwitchColor() {
+  clearInterval(timerId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function toColorBody() {
+  refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+}
